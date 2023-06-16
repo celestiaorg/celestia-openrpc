d2e0c1d908e06261dd361a65a08eb568c6390dfc
Javed Khan <tuxcanfly@gmail.com>
2023-06-16 10:34:30 -0700

    types: add ExtendedHeader json marshal / unmarshal

--- types/header/header.go
+++ types/header/header.go
@@ -1,6 +1,11 @@
 package header
 
-import "github.com/rollkit/celestia-openrpc/types/core"
+import (
+	"encoding/json"
+
+	"github.com/rollkit/celestia-openrpc/types/core"
+	tmjson "github.com/tendermint/tendermint/libs/json"
+)
 
 // RawHeader is an alias to core.Header. It is
 // "raw" because it is not yet wrapped to include
@@ -15,6 +20,58 @@ type ExtendedHeader struct {
 	Commit       *core.Commit            `json:"commit"`
 	ValidatorSet *core.ValidatorSet      `json:"validator_set"`
 	DAH          *DataAvailabilityHeader `json:"dah"`
+}
+
+// MarshalJSON marshals an ExtendedHeader to JSON. The ValidatorSet is wrapped with amino encoding,
+// to be able to unmarshal the crypto.PubKey type back from JSON.
+func (eh *ExtendedHeader) MarshalJSON() ([]byte, error) {
+	type Alias ExtendedHeader
+	validatorSet, err := tmjson.Marshal(eh.ValidatorSet)
+	if err != nil {
+		return nil, err
+	}
+	rawHeader, err := tmjson.Marshal(eh.RawHeader)
+	if err != nil {
+		return nil, err
+	}
+	return json.Marshal(&struct {
+		RawHeader    json.RawMessage `json:"header"`
+		ValidatorSet json.RawMessage `json:"validator_set"`
+		*Alias
+	}{
+		ValidatorSet: validatorSet,
+		RawHeader:    rawHeader,
+		Alias:        (*Alias)(eh),
+	})
+}
+
+// UnmarshalJSON unmarshals an ExtendedHeader from JSON. The ValidatorSet is wrapped with amino
+// encoding, to be able to unmarshal the crypto.PubKey type back from JSON.
+func (eh *ExtendedHeader) UnmarshalJSON(data []byte) error {
+	type Alias ExtendedHeader
+	aux := &struct {
+		RawHeader    json.RawMessage `json:"header"`
+		ValidatorSet json.RawMessage `json:"validator_set"`
+		*Alias
+	}{
+		Alias: (*Alias)(eh),
+	}
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	valSet := new(core.ValidatorSet)
+	if err := tmjson.Unmarshal(aux.ValidatorSet, valSet); err != nil {
+		return err
+	}
+	rawHeader := new(RawHeader)
+	if err := tmjson.Unmarshal(aux.RawHeader, rawHeader); err != nil {
+		return err
+	}
+
+	eh.ValidatorSet = valSet
+	eh.RawHeader = *rawHeader
+	return nil
 }
 
 type DataAvailabilityHeader = core.DataAvailabilityHeader
