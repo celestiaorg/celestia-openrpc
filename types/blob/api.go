f8ac0ed0073b8336669394c42a71a8713c9af3ee
Ryan <ryanford@poluglottos.com>
2024-05-06 15:42:37 +0200

    modularization and go-da

---
+++ types/blob/api.go
@@ -0,0 +1,15 @@
+package blob
+
+import (
+	"context"
+
+	"github.com/celestiaorg/celestia-openrpc/types/share"
+)
+
+type API struct {
+	Submit   func(context.Context, []*Blob, float64) (uint64, error)                          `perm:"write"`
+	Get      func(context.Context, uint64, share.Namespace, Commitment) (*Blob, error)        `perm:"read"`
+	GetAll   func(context.Context, uint64, []share.Namespace) ([]*Blob, error)                `perm:"read"`
+	GetProof func(context.Context, uint64, share.Namespace, Commitment) (*Proof, error)       `perm:"read"`
+	Included func(context.Context, uint64, share.Namespace, *Proof, Commitment) (bool, error) `perm:"read"`
+}
---
+++ types/state/api.go
@@ -0,0 +1,83 @@
+package state
+
+import (
+	"context"
+	"github.com/celestiaorg/celestia-openrpc/types/blob"
+)
+
+type API struct {
+	AccountAddress    func(ctx context.Context) (Address, error)                `perm:"read"`
+	IsStopped         func(ctx context.Context) bool                            `perm:"read"`
+	Balance           func(ctx context.Context) (*Balance, error)               `perm:"read"`
+	BalanceForAddress func(ctx context.Context, addr Address) (*Balance, error) `perm:"read"`
+	Transfer          func(
+		ctx context.Context,
+		to AccAddress,
+		amount,
+		fee Int,
+		gasLimit uint64,
+	) (*TxResponse, error) `perm:"write"`
+	SubmitTx         func(ctx context.Context, tx Tx) (*TxResponse, error) `perm:"write"`
+	SubmitPayForBlob func(
+		ctx context.Context,
+		fee Int,
+		gasLim uint64,
+		blobs []*blob.Blob,
+	) (*TxResponse, error) `perm:"write"`
+	CancelUnbondingDelegation func(
+		ctx context.Context,
+		valAddr ValAddress,
+		amount,
+		height,
+		fee Int,
+		gasLim uint64,
+	) (*TxResponse, error) `perm:"write"`
+	BeginRedelegate func(
+		ctx context.Context,
+		srcValAddr,
+		dstValAddr ValAddress,
+		amount,
+		fee Int,
+		gasLim uint64,
+	) (*TxResponse, error) `perm:"write"`
+	Undelegate func(
+		ctx context.Context,
+		delAddr ValAddress,
+		amount,
+		fee Int,
+		gasLim uint64,
+	) (*TxResponse, error) `perm:"write"`
+	Delegate func(
+		ctx context.Context,
+		delAddr ValAddress,
+		amount,
+		fee Int,
+		gasLim uint64,
+	) (*TxResponse, error) `perm:"write"`
+	QueryDelegation func(
+		ctx context.Context,
+		valAddr ValAddress,
+	) (*QueryDelegationResponse, error) `perm:"read"`
+	QueryUnbonding func(
+		ctx context.Context,
+		valAddr ValAddress,
+	) (*QueryUnbondingDelegationResponse, error) `perm:"read"`
+	QueryRedelegations func(
+		ctx context.Context,
+		srcValAddr,
+		dstValAddr ValAddress,
+	) (*QueryRedelegationsResponse, error) `perm:"read"`
+	GrantFee func(
+		ctx context.Context,
+		grantee AccAddress,
+		amount,
+		fee Int,
+		gasLim uint64,
+	) (*TxResponse, error) `perm:"write"`
+	RevokeGrantFee func(
+		ctx context.Context,
+		grantee AccAddress,
+		fee Int,
+		gasLim uint64,
+	) (*TxResponse, error) `perm:"write"`
+}
